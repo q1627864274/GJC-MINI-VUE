382268f5b19e2f4acc122e180694effc3338100c
gjcfe <1627864274@qq.com>
2024-09-26 15:46:13 +0800

    实现getCurrentInstance

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -5,36 +5,38 @@ import { PublicInstanceProxyHandlers } from "./componentPublicInstance";
 import { initSlots } from "./componentSlots";
 
 export function createComponentInstance(vnode) {
-  const component = { 
-    vnode, 
+  const component = {
+    vnode,
     type: vnode.type,
     setupState: {},
     props: {},
     slots: {},
-    emit: () => {}
-  }; 
-  component.emit = emit.bind(null, component) as any
+    emit: () => {},
+  };
+  component.emit = emit.bind(null, component) as any;
   return component;
 }
 export function setupComponent(instance) {
   //  TODO
-  initProps(instance, instance.vnode.props)
-  initSlots(instance, instance.vnode.children)
+  initProps(instance, instance.vnode.props);
+  initSlots(instance, instance.vnode.children);
   setupStatefulComponent(instance);
 }
 
 function setupStatefulComponent(instance: any) {
   const Component = instance.type;
- 
-  // {} -> ctx
-  instance.proxy = new Proxy({_: instance}, PublicInstanceProxyHandlers)
 
+  // {} -> ctx
+  instance.proxy = new Proxy({ _: instance }, PublicInstanceProxyHandlers);
 
   const { setup } = Component;
   if (setup) {
+    setCurrentInstance(instance)
     // function object
-    const setupResult = setup(shallowReadonly(instance.props), {emit: instance.emit});
-
+    const setupResult = setup(shallowReadonly(instance.props), {
+      emit: instance.emit,
+    });
+    setCurrentInstance(null)
     handleSetupResult(instance, setupResult);
   }
 }
@@ -47,9 +49,15 @@ function handleSetupResult(instance, setupResult: any) {
   finishComponentSetup(instance);
 }
 function finishComponentSetup(instance: any) {
-    const Component = instance.type
-    if (Component.render) {
-        instance.render = Component.render
-    }
+  const Component = instance.type;
+  if (Component.render) {
+    instance.render = Component.render;
+  }
 }
-
+let currentInstance = null;
+export function getCurrentInstance() {
+  return currentInstance;
+}
+export function setCurrentInstance(instance:any){
+  currentInstance = instance
+}
